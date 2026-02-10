1678331c68770c4224098775586fe60b103fc9d3
sparklingkarthi2005-hash <sparklingkarthi2005@gmail.com>
2026-02-10 21:25:07 +0530

    Add files via upload

---
+++ tax.js
@@ -0,0 +1,37 @@
+function tax ()
+{
+    let a=document.getElementById("a").value;
+    if (a>=1500001)
+    {
+    let result=140000+(a-1500000)*(30/100);
+    document.getElementById("result").innerHTML="Tax amount : " + result;
+    }
+    else if (a>=1200001)
+    {
+    let result=80000+(a-1200000)*(20/100);
+    document.getElementById("result").innerHTML="Tax amount : " + result;
+    }
+    else if (a>=1000001)
+    {
+    let result=50000+(a-1000000)*(15/100);
+    document.getElementById("result").innerHTML="Tax amount : " + result;
+    }
+    else if (a>=700001)
+    {
+    let result=20000+(a-700000)*(10/100);
+    document.getElementById("result").innerHTML="Tax amount : "+  result;
+    }
+    else if (a>=300001)
+    {
+    let result=a*5/100;
+    document.getElementById("result").innerHTML="Tax amount : "+ result;
+    }
+    else if (a>=1)
+    {
+    result.innerHTML="No tax amount";
+    }
+    else
+    {
+    result.innerHTML="Enter the income amount for calculation";
+    }
+}
